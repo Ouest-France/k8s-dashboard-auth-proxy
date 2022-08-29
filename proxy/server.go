9f1ec7068692820ee6785a94e14328df601da92e
Pablo RUTH <contact@pablo-ruth.fr>
2022-08-29 11:42:00 +0200

    move middleware before routes

--- proxy/server.go
+++ proxy/server.go
@@ -13,17 +13,19 @@ func Server(loginURL, guestClusterName, proxyURL string, debug bool) error {
 	// Remove timestamp from logs
 	log.SetFlags(0)
 
-	// Create router and register handlers
+	// Create router
 	r := chi.NewRouter()
-	r.HandleFunc("/*", proxyHandler(proxyURL))
-	r.Get("/login", loginGetHandler)
-	r.Post("/login", loginPostHandler(loginURL, guestClusterName))
-	r.Get("/logout", logoutGetHandler)
 
 	// Enable request logging if debug is enabled
 	if debug {
 		r.Use(middleware.Logger)
 	}
+
+	// Add routes
+	r.HandleFunc("/*", proxyHandler(proxyURL))
+	r.Get("/login", loginGetHandler)
+	r.Post("/login", loginPostHandler(loginURL, guestClusterName))
+	r.Get("/logout", logoutGetHandler)
 
 	// Serve requests
 	return http.ListenAndServe(":8080", r)
