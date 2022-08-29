b98f4018a24ad8ac3922c8d206fe429ff90f9fe2
Pablo RUTH <contact@pablo-ruth.fr>
2022-08-29 11:34:19 +0200

    add debug mode with request logging

--- proxy/server.go
+++ proxy/server.go
@@ -5,9 +5,10 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
-func Server(loginURL, guestClusterName, proxyURL string) error {
+func Server(loginURL, guestClusterName, proxyURL string, debug bool) error {
 
 	// Remove timestamp from logs
 	log.SetFlags(0)
@@ -18,6 +19,11 @@ func Server(loginURL, guestClusterName, proxyURL string) error {
 	r.Get("/login", loginGetHandler)
 	r.Post("/login", loginPostHandler(loginURL, guestClusterName))
 	r.Get("/logout", logoutGetHandler)
+
+	// Enable request logging if debug is enabled
+	if debug {
+		r.Use(middleware.Logger)
+	}
 
 	// Serve requests
 	return http.ListenAndServe(":8080", r)
